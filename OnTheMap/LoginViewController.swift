bdacf06d631ce3280946f0825da2e9241dfa9c08
mi <infogeyser@ms-imac.local>
2017-04-08 22:12:12 -0400

    Added code to viewDidLoad

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -14,6 +14,9 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://www.udacity.com")
+        let requestObj = URLRequest(url: url! as URL)
+        udacityWebView.loadRequest(requestObj)
     }
     
     //Try loading this way as well, but try from viewDidLoad first
