aed0ef54eca99b8dc6cc4fe1a2ebaf49c4d0d215
Jason <jisler3@gatech.edu>
2017-04-14 14:34:27 -0400

    Added IBOutlets and some starter code for the loginViewController.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
 
     
     override func viewDidLoad() {
@@ -17,6 +24,23 @@ class LoginViewController: UIViewController {
         view.layer.borderWidth = 1.0
         view.layer.cornerRadius = 10.0
     }
+    
+    
+//    let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+//    request.httpMethod = "POST"
+//    request.addValue("application/json", forHTTPHeaderField: "Accept")
+//    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//    request.httpBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".data(using: String.Encoding.utf8)
+//    let session = URLSession.shared
+//    let task = session.dataTask(with: request as URLRequest) { data, response, error in
+//        if error != nil { // Handle error…
+//            return
+//        }
+//        let range = Range(5..<data!.count)
+//        let newData = data?.subdata(in: range) /* subset response data! */
+//        print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
+//    }
+//    task.resume()
     
 
     
