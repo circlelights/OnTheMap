44269cfd96beb33261765f1e1fc130f2164b1b80
Jason <jisler3@gatech.edu>
2017-04-18 17:56:32 -0400

    Added escapedParameters method.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -42,6 +42,29 @@ class LoginViewController: UIViewController {
 //    }
 //    task.resume()
     
+    private func escapedParameters(_ parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
+    }
 
     
 }
