5adab4c85a79f10e5de6e4c40bd4045cb5cd470d
Jason <jisler3@gatech.edu>
2017-04-27 13:10:20 -0400

    Added the Constants.swift file.

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,50 @@
+//
+//  Constants.swift
+//  OnTheMap
+//
+//  Created by Developer2017 on 4/27/17.
+//  Copyright © 2017 Developer2017. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Constants
+
+struct Constants {
+    
+    // MARK: TMDB
+    struct TMDB {
+        static let ApiScheme = "https"
+        static let ApiHost = "udacity.com/"
+        static let ApiPath = "api/session"
+    }
+    
+    // MARK: TMDB Parameter Keys
+    struct TMDBParameterKeys {
+        static let ApiKey = "api_key"
+        static let RequestToken = "request_token"
+        static let SessionID = "session_id"
+        static let Username = "username"
+        static let Password = "password"
+    }
+    
+    // MARK: TMDB Parameter Values
+    struct TMDBParameterValues {
+        static let ApiKey = "YOUR_API_KEY_HERE"
+    }
+    
+    // MARK: TMDB Response Keys
+    struct TMDBResponseKeys {
+        static let Title = "title"
+        static let ID = "id"
+        static let PosterPath = "poster_path"
+        static let StatusCode = "status_code"
+        static let StatusMessage = "status_message"
+        static let SessionID = "session_id"
+        static let RequestToken = "request_token"
+        static let Success = "success"
+        static let UserID = "id"
+        static let Results = "results"
+}
+}
+
