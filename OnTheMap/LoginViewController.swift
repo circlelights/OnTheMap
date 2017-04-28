ece70dbadefd09996c95ddc96503211ef6c9930d
Jason <jisler3@gatech.edu>
2017-04-28 11:41:53 -0400

    Added the code for deleting a session.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -82,6 +82,30 @@ private func getRequestToken() {
         task.resume()
 }
 
+private func deleteSession() {
+        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "DELETE"
+        var xsrfCookie: HTTPCookie? = nil
+        let sharedCookieStorage = HTTPCookieStorage.shared
+        for cookie in sharedCookieStorage.cookies! {
+        if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let range = Range(5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
+        }
+        task.resume()
+}
+
+
 // MARK: - LoginViewController (Configure UI)
 
 private extension LoginViewController {
@@ -101,27 +125,6 @@ private extension LoginViewController {
         }
     }
 
-//    let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
-//    request.httpMethod = "DELETE"
-//    var xsrfCookie: HTTPCookie? = nil
-//    let sharedCookieStorage = HTTPCookieStorage.shared
-//    for cookie in sharedCookieStorage.cookies! {
-//    if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
-//    }
-//    if let xsrfCookie = xsrfCookie {
-//        request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
-//    }
-//    let session = URLSession.shared
-//    let task = session.dataTask(with: request as URLRequest) { data, response, error in
-//        if error != nil { // Handle error…
-//            return
-//        }
-//        let range = Range(5..<data!.count)
-//        let newData = data?.subdata(in: range) /* subset response data! */
-//        print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
-//    }
-//    task.resume()
-//    
     private func escapedParameters(_ parameters: [String:AnyObject]) -> String {
         
         if parameters.isEmpty {
